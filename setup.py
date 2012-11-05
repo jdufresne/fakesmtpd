343ea9899c44d99164524c5b01062552bec424d4
Jon Dufresne <jon@jondufresne.org>
2012-11-04 17:58:03 -0800

    Added description to setup and bumped version

--- setup.py
+++ setup.py
@@ -3,18 +3,15 @@ from distutils.core import setup
 
 def main():
     name = 'fakesmtpd'
-    version = '0.0.3'
+    version = '0.0.4'
+    url = 'http://pypi.python.org/packages/source/f/{name}/{name}-{version}.tar.gz'
 
-    setup(
-        name=name,
-        version=version,
-        url='http://pypi.python.org/pypi/{name}'.format(name=name),
-        description="Fake SMTP Server",
-        download_url='http://pypi.python.org/packages/source/f/{name}/{name}-{version}.tar.gz'.format(
-            name=name,
-            version=version,
-        ),
-        classifiers=[
+    setup(name=name,
+          version=version,
+          url='http://pypi.python.org/pypi/{name}'.format(name=name),
+          description=open('README.md').read(),
+          download_url=url.format(name=name, version=version),
+          classifiers=[
             'Development Status :: 3 - Alpha',
             'Environment :: No Input/Output (Daemon)',
             'Intended Audience :: Developers',
@@ -22,10 +19,9 @@ def main():
             'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
             'Programming Language :: Python',
             'Topic :: Communications :: Email :: Mail Transport Agents',
-        ],
-        scripts=['fakesmtpd'],
-        data_files=[('/lib/systemd/system', ['fakesmtpd.service'])]
-    )
+          ],
+          scripts=['fakesmtpd'],
+          data_files=[('/lib/systemd/system', ['fakesmtpd.service'])])
 
 
 if __name__ == '__main__':
