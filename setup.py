9d8b5193d2e122349ec1232298266993b94285f0
Jon Dufresne <jon@jondufresne.org>
2012-11-04 18:16:39 -0800

    Added a long description

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from distutils.core import setup
 
 def main():
     name = 'fakesmtpd'
-    version = '0.0.5'
+    version = '0.0.6'
     url = 'http://pypi.python.org/packages/source/f/{name}/{name}-{version}.tar.gz'
 
     setup(name=name,
           version=version,
           url='http://pypi.python.org/pypi/{name}'.format(name=name),
-          description=open('README.rst').read(),
+          descriptoin='Fake SMTP server',
+          long_description=open('README.rst').read(),
           author='Jon Dufresne',
           author_email='jon@jondufresne.org',
           download_url=url.format(name=name, version=version),
